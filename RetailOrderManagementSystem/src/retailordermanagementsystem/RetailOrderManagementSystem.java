a540be73da99678cfc305c1b7873f6a10dd7be21
Maxine <ms.l@hotmail.com>
2021-07-02 19:48:53 +0800

    Initial Commit

---
+++ RetailOrderManagementSystem/src/retailordermanagementsystem/RetailOrderManagementSystem.java
@@ -0,0 +1,17 @@
+
+package retailordermanagementsystem;
+
+/**
+ *
+ * @author Lai Mei Sim, Koh Ya Wen
+ */
+public class RetailOrderManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
